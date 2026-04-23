32341651a38e1c2301b188e559af3944321b77fd
Anushka Bhatia <227209142+AnushkaBhatia1229@users.noreply.github.com>
2026-04-23 18:12:36 +0530

    Time: 221 ms (19.69%), Space: 55.7 MB (47.64%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.py
@@ -0,0 +1,35 @@
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        
+        mp = {}  # num -> [cnt, last_i, acc_d]
+        pre = [0] * n
+        suf = [0] * n
+
+        for i in range(n):
+            x = nums[i]
+            if x in mp:
+                pre[i] = mp[x][2] + (mp[x][0] * (i - mp[x][1]))
+                mp[x][0] += 1
+                mp[x][1] = i
+                mp[x][2] = pre[i]
+            else:
+                mp[x] = [1, i, 0]
+
+        mp = {}
+
+        for i in range(n - 1, -1, -1):
+            x = nums[i]
+            if x in mp:
+                suf[i] = mp[x][2] + (mp[x][0] * (mp[x][1] - i))
+                mp[x][0] += 1
+                mp[x][1] = i
+                mp[x][2] = suf[i]
+            else:
+                mp[x] = [1, i, 0]
+
+        res = [0] * n
+        for i in range(n):
+            res[i] = pre[i] + suf[i]
+
+        return res
