01d4cc2544e42794a945379dc60c21af55c82573
Anushka Bhatia <227209142+AnushkaBhatia1229@users.noreply.github.com>
2026-06-03 13:06:22 +0530

    Time: 47 ms (61.88%), Space: 19 MB (98.05%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        # Bottom Up - Tabulation
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        prev = 0
+        cur = 1
+
+        for i in range(2, n + 1):
+            prev, cur = cur, prev + cur
+
+        return cur
+
+        # Time: O(n)
+        # Space: O(1)
